a096fc780b09ee55cca75e9c55f3d2bc6f46dcc9
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-08-11 00:42:45 +0000

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,22 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(timeString){
+  let time = timeString.split(':');
+  console.log(time[0]);
+  let hour = parseInt(time[0]);
+
+  if (hour < 12){
+    return 'Good Morning'
+  }
+  else if (hour >= 12 && hour <= 17 ) {
+    return 'Good Afternoon'
+  }
+  else {
+    return "Good Evening"
+  }
+};
+
+function displayMessage(string){
+  document.getElementById("greeting").innerText = string;
+}
